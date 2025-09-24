284a61752c692c02373e6e4b8bb051cde8e7c0a9
Arman Ul Haq <armanulhaq@Armans-MacBook-Air.local>
2025-09-24 15:45:08 +0530

    initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="flex flex-col min-h-screen bg-fuchsia-50"></div>
+        </>
+    );
+}
+
+export default App;
